9bc7894aabcbfb9214c8a407ddf626c48bdc6189
kassko <meiacapo@free.fr>
2015-05-28 03:01:36 +0200

    Update for coding standard

--- tests/ClassMetadataLoader/Exception/NotFoundLoaderExceptionTest.php
+++ tests/ClassMetadataLoader/Exception/NotFoundLoaderExceptionTest.php
@@ -11,62 +11,62 @@ use Kassko\DataMapper\ClassMetadataLoader\Exception\NotFoundLoaderException;
  */
 class NotFoundLoaderExceptionTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * @var NotFoundLoaderException
-     */
-    protected $exception;
+        /**
+         * @var NotFoundLoaderException
+         */
+        protected $exception;
 
-    /**
-     * @var Fixture\LoadingCriteria|\PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $loadingCriteriaMock;
+        /**
+         * @var Fixture\LoadingCriteria|\PHPUnit_Framework_MockObject_MockObject
+         */
+        protected $loadingCriteriaMock;
 
-    /**
-     * @return void
-     */
-    public function setUp()
-    {
-        $this->loadingCriteriaMock = $this->getMockBuilder(
-            'Kassko\DataMapperTest\ClassMetadataLoader\Fixture\LoadingCriteria'
-        )->getMock();
+        /**
+         * @return void
+         */
+        public function setUp()
+        {
+                $this->loadingCriteriaMock = $this->getMockBuilder(
+                        'Kassko\DataMapperTest\ClassMetadataLoader\Fixture\LoadingCriteria'
+                )->getMock();
 
-        $resourcePath = 'testResourcePath';
-        $resourceType = 'testResourceType';
-        $resourceClass = 'testResourceClass';
-        $resourceMethod = 'testResourceMethod';
-        $this->loadingCriteriaMock->expects($this->once())
-                                  ->method('getResourcePath')
-                                  ->willReturn($resourcePath);
-        $this->loadingCriteriaMock->expects($this->once())
-                                  ->method('getResourceType')
-                                  ->willReturn($resourceType);
-        $this->loadingCriteriaMock->expects($this->once())
-                                  ->method('getResourceClass')
-                                  ->willReturn($resourceClass);
-        $this->loadingCriteriaMock->expects($this->once())
-                                  ->method('getResourceMethod')
-                                  ->willReturn($resourceMethod);
+                $resourcePath = 'testResourcePath';
+                $resourceType = 'testResourceType';
+                $resourceClass = 'testResourceClass';
+                $resourceMethod = 'testResourceMethod';
+                $this->loadingCriteriaMock->expects($this->once())
+                                                                    ->method('getResourcePath')
+                                                                    ->willReturn($resourcePath);
+                $this->loadingCriteriaMock->expects($this->once())
+                                                                    ->method('getResourceType')
+                                                                    ->willReturn($resourceType);
+                $this->loadingCriteriaMock->expects($this->once())
+                                                                    ->method('getResourceClass')
+                                                                    ->willReturn($resourceClass);
+                $this->loadingCriteriaMock->expects($this->once())
+                                                                    ->method('getResourceMethod')
+                                                                    ->willReturn($resourceMethod);
 
-        $this->exception = new NotFoundLoaderException($this->loadingCriteriaMock);
-    }
+                $this->exception = new NotFoundLoaderException($this->loadingCriteriaMock);
+        }
 
-    /**
-     * @test
-     */
-    public function instanceOfRuntimeException()
-    {
-        $this->assertInstanceOf('\RuntimeException', $this->exception);
-    }
+        /**
+         * @test
+         */
+        public function instanceOfRuntimeException()
+        {
+                $this->assertInstanceOf('\RuntimeException', $this->exception);
+        }
 
-    /**
-     * @test
-     */
-    public function constructorValidateCalls()
-    {
-        $this->assertEquals(
-            'No loader found or no loader satisfies the following criteria: [resourcePath="testResourcePath"] - ' .
-            '[resourceType="testResourceType"] - [resourceClass="testResourceClass"] - [resourceMethod="testResourceMethod"]',
-            $this->exception->getMessage()
-        );
-    }
+        /**
+         * @test
+         */
+        public function constructorValidateCalls()
+        {
+                $this->assertEquals(
+                        'No loader found or no loader satisfies the following criteria: [resourcePath="testResourcePath"] - ' .
+                        '[resourceType="testResourceType"] - [resourceClass="testResourceClass"] - [resourceMethod="testResourceMethod"]',
+                        $this->exception->getMessage()
+                );
+        }
 }
