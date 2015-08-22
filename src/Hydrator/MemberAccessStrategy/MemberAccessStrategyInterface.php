433cc6aae107918f9910a60ee4c7bfdd7e660211
kassko <meiacapo@free.fr>
2015-08-22 23:40:32 +0200

    Just add omment on unused method

--- src/Hydrator/MemberAccessStrategy/MemberAccessStrategyInterface.php
+++ src/Hydrator/MemberAccessStrategy/MemberAccessStrategyInterface.php
@@ -14,6 +14,12 @@ interface MemberAccessStrategyInterface
    public function prepare($object, ClassMetadata $metadata);
    public function getValue($object, $fieldName);
    public function setValue($value, $object, $fieldName);
+   /**
+    * Unused method. To be remove.
+    */
    public function setSingleAssociation($subObject, $object, $fieldName);
+   /**
+    * Unused method. To be remove.
+    */
    public function setCollectionAssociation(array $subObjects, $object, $fieldName, $adderPart);
 }
