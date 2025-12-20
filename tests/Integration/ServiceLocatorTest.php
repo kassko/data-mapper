4338b393651ea9b0de63597c80f869e1931a5661
kassko <kassko@users.noreply.github.com>
2025-12-20 12:33:59 +0000

    Fix ServiceLocatorTest exception test to use MultiPropDataSource

--- tests/Integration/ServiceLocatorTest.php
+++ tests/Integration/ServiceLocatorTest.php
@@ -75,12 +75,14 @@ class ServiceLocatorTest extends TestCase
     public function testServiceLocatorThrowsExceptionWithoutContainer(): void
     {
         // Create an entity that uses service locator pattern
-        $entity = new class(1) {
+        $entity = new 
+        #[MultiPropDataSource(id: 'personData', class: '@person.data_source', method: 'getData', args: ['#id'])]
+        class(1) {
             use LoadableTrait;
 
             private int $id;
 
-            #[DataSource(class: '@person.data_source', method: 'getData', args: ['#id'])]
+            #[DataSourceRef(id: 'personData')]
             private ?string $name = null;
 
             public function __construct(int $id)
