27165bdc049a47c52946fe5fba568e9e3b0d78e8
kassko <kassko@users.noreply.github.com>
2025-12-20 12:21:54 +0000

    Fix tests to use new MultiPropDataSource and DataSourceRef pattern

--- tests/Integration/ServiceLocatorTest.php
+++ tests/Integration/ServiceLocatorTest.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace Kassko\DataMapper\Tests\Integration;
 
-use Kassko\DataMapper\Attribute\DataSource;
+use Kassko\DataMapper\Attribute\MultiPropDataSource;
+use Kassko\DataMapper\Attribute\DataSourceRef;
 use Kassko\DataMapper\DataMapper;
 use Kassko\DataMapper\ObjectExtension\LoadableTrait;
 use Kassko\Sample\PersonDataSource;
@@ -32,15 +33,17 @@ class ServiceLocatorTest extends TestCase
         };
 
         // Create an entity that uses service locator pattern
-        $entity = new class(1) {
+        $entity = new 
+        #[MultiPropDataSource(id: 'personData', class: '@person.data_source', method: 'getData', args: ['#id'])]
+        class(1) {
             use LoadableTrait;
 
             private int $id;
 
-            #[DataSource(class: '@person.data_source', method: 'getData', args: ['#id'])]
+            #[DataSourceRef(id: 'personData')]
             private ?string $name = null;
 
-            #[DataSource(class: '@person.data_source', method: 'getData', args: ['#id'])]
+            #[DataSourceRef(id: 'personData')]
             private ?string $email = null;
 
             public function __construct(int $id)
