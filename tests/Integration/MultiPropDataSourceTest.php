2bb2ebb0903751b8fb907defce50f023cf4d397f
kassko <kassko@users.noreply.github.com>
2025-12-26 00:22:50 +0000

    Remove unused namespace

--- tests/Integration/MultiPropDataSourceTest.php
+++ tests/Integration/MultiPropDataSourceTest.php
@@ -7,7 +7,6 @@ namespace Kassko\DataMapper\Tests\Integration;
 use Kassko\DataMapper\DataMapper;
 use Kassko\DataMapper\Registry\LoaderRegistry;
 use Kassko\Sample\CustomerWithMultiPropDataSource;
-use Kassko\Sample\MultiPropDSOnPropCustomer;
 use PHPUnit\Framework\TestCase;
 
 class MultiPropDataSourceTest extends TestCase
