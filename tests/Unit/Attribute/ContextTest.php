d796d84a12319c86e734d3d4c3d260f380cdeaab
root <meiacapo@free.fr>
2025-12-27 10:05:14 +0000

    Fix compliance

--- tests/Unit/Attribute/ContextTest.php
+++ tests/Unit/Attribute/ContextTest.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of Data Mapper.
+ *
+ * Copyright 2025 kassko 
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE and NOTICE files that were distributed with this source code.
+ */
+
 namespace Kassko\DataMapper\Tests\Unit\Attribute;
 
 use Kassko\DataMapper\Attribute\Context;
